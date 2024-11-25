db7ce88c2a4cc99968557c95598a1f8776c4f055
r17_iNSANE <rakshith.pj@santec.com>
2024-11-25 15:39:31 +0900

    Create test_query.py

---
+++ testsuite/test_query.py
@@ -0,0 +1,13 @@
+import pytest
+from ftd2xxhelper import Ftd2xxhelper
+
+devices = Ftd2xxhelper.list_devices()
+
+command = 'POW?'
+
+
+def test_query_idn():
+    helper = Ftd2xxhelper(devices[0].SerialNumber)
+    response = helper.query(command=command)
+    assert isinstance(response, str)
+    assert len(response) > 0
