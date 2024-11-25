57e87bd528e931a0520f9971b7bcc7f4c78b55b9
r17_iNSANE <rakshith.pj@santec.com>
2024-11-25 15:25:28 +0900

    Create test_list_devices.py

---
+++ testsuite/test_list_devices.py
@@ -0,0 +1,8 @@
+import pytest
+from ftd2xxhelper import Ftd2xxhelper
+
+
+def test_list_devices():
+    devices = Ftd2xxhelper.list_devices()
+    assert isinstance(devices, list)
+    assert len(devices) >= 0
