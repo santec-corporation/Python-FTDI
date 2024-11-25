109e5ec0a718e86ca9f37b5f5a79c05a39a67ca0
r17_iNSANE <rakshith.pj@santec.com>
2024-11-25 15:30:39 +0900

    Create test_initialize_with_serial.py

---
+++ testsuite/test_initialize_with_serial.py
@@ -0,0 +1,11 @@
+import pytest
+from ftd2xxhelper import Ftd2xxhelper
+
+serial_number = 23110067
+serial_number_to_byte = str(serial_number).encode('utf-8')
+
+
+def test_initialize_with_serial():
+    helper = Ftd2xxhelper(serial_number_to_byte)
+    assert helper._ft_handle is not None
+    assert helper._last_connected_serial_number.decode('ascii') == str(serial_number)
