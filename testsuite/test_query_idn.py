305954f157c13e97cb17601b7933e04f0e9d2ddd
r17_iNSANE <rakshith.pj@santec.com>
2024-11-25 15:39:28 +0900

    Create test_query_idn.py

---
+++ testsuite/test_query_idn.py
@@ -0,0 +1,11 @@
+import pytest
+from ftd2xxhelper import Ftd2xxhelper
+
+devices = Ftd2xxhelper.list_devices()
+
+
+def test_query_idn():
+    helper = Ftd2xxhelper(devices[0].SerialNumber)
+    response = helper.query_idn()
+    assert isinstance(response, str)
+    assert len(response) > 0
