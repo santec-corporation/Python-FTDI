7ebfcae8eead74e51835d080b1959cebba50cd41
r17_iNSANE <rakshith.pj@santec.com>
2024-11-26 17:10:00 +0900

    Create test_write.py

---
+++ testsuite/test_write.py
@@ -0,0 +1,18 @@
+import pytest
+from ftd2xxhelper import Ftd2xxhelper
+
+devices = Ftd2xxhelper.list_devices()
+
+wav = float(1515)
+write_wav_command = f'WAV {wav}'
+query_wav_command = 'WAV?'
+
+
+def test_write():
+    helper = Ftd2xxhelper(devices[0].SerialNumber)
+
+    helper.write(command=write_wav_command)
+
+    query_response = float(helper.query(query_wav_command))
+
+    assert query_response == wav, f"Expected {wav}, but got {query_response}"
